f208d98f2708b725c13db54e2d97dfcfbe435c89
gphil1012 <gphillips.1012@gmail.com>
2017-01-16 20:02:48 -0500

    first program

---
+++ hellowWorld.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello world!\n";
+    cout << "This is my first time using GitHub. It seems pretty cool.\n";
+    cout << "That is everything you need to know about me.\n";
+
+
+    return 0;
+}
+
+
