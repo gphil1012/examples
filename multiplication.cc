5d897c86d9df3eabf9a8f38b547bcedce8d1f6f7
capalmer1013 <capalmer1013@gmail.com>
2017-01-25 22:08:51 -0500

    for loops and some output formatting

---
+++ multiplication.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	for(int i = 1; i <= 10; i++){
+
+		for(int j = 1; j <= 10; j++){
+			cout << setw(4);
+			cout << i*j << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
+
+
--- warmup1.cc
+++ warmup1.cc
@@ -1,36 +1,39 @@
-#include <iostream>
+#include <iostream> // include external files for input and output
 #include <cmath>
 
 using namespace std;
 
-int main(){
+int main(){  // defining a function called main which takes no inputs and returns an int
     // define varibles
-    float inputA;
+    float inputA;  // declaring 3 float variables
     float inputB;
-    float inputC;
-    float inputTax = 1.07;
+    float inputC;  // undefined
 
+    float inputTax = 1.07;  // assigning a variable
+
+    // more undefined variables
     int   resultCents;
-
     float result;
 
     // take inputs
-    cin >> inputA;
-    cin >> inputB;
-    cin >> inputC;
+    cin >> inputA;  // reading the value from cin (Console INput)
+    cin >> inputB;  // storing that value as a float in previously
+    cin >> inputC;  // declared variables
 
     // do computation
 
     // comput the sum of inputA, B, and C
     // store sum in result variable
 
-    result = inputA + inputB + inputC;
+    result = inputA + inputB + inputC;  // adding 3 floats storing in result
 
-	resultCents = result * 100;
+	resultCents = result * 100;  // multiplying result by 100 casting to an int
 
-	result = round(resultCents*inputTax)/100;
+	result = round(resultCents*inputTax)/100;  // c++ casts on assignment
 
     // print output
     cout << '\n'
     << result << '\n';
-}
+
+    return 0; // program completed successfully
+}  // end the main function
